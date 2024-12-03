85564a1d963de8ae9d155bbaa72cf5ab5f26c209
ss0n1c <lls.s0n1cll@gamil.com>
2024-12-03 22:28:23 +0200

    create layouts

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="gird">
+    <aside>menu</aside>
+    <div>
+        <slot />
+    </div>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.grid{
+    display: grid;
+    min-width: 100vh;
+    grid-template-columns: 1fr 6fr;
+}
+</style>
