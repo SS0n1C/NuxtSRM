290c7ed1634892de663310608c4a7cdbd3a8f30d
ss0n1c <lls.s0n1cll@gamil.com>
2025-01-18 13:32:34 +0200

    smal change in style on login page

--- pages/login.vue
+++ pages/login.vue
@@ -35,11 +35,13 @@ const regist = async() =>{
 </script>
 
 .<template>
-    <section class="flex items-center justify-center min-h-screen w-full bg-slate-200">
-      <div class="bg-red-300 flex items-center justify-center flex-col min-w-72 py-2" >
-          <h3 class="text-center mb-5">login</h3>
-          <form action="">
-              <input type="email" 
+    <section class="flex items-center justify-center min-h-screen w-full bg-[#0c4a6e]">
+      <div class="bg-white flex items-center flex-col min-w-96 py-2 min-h-72 rounded-3xl" >
+          <div><h3 class="text-center text-xl uppercase font-semibold underline mb-5">login</h3></div>
+          <form action="" class="grow flex">
+          <div class=" flex flex-col p-3 min-h-full">
+            <div class="grow">  
+            <input type="email" 
                   placeholder="email"
                   v-model="emailRef">
               <input type="password" 
@@ -48,11 +50,13 @@ const regist = async() =>{
               <input type="text" 
                   placeholder="name"
                   v-model="nameRef">
-              <div class="flex items-center justify-center gap-x-10">
+            </div>
+              <div class="flex items-center justify-center gap-x-10 ">
                   <button type="button" class="but" @click="login">login</button>
                   <button type="button" class="but" @click="regist">registration</button>
               </div>
-          </form>
+        </div>
+        </form>
       </div>
     </section>
   </template>
@@ -63,12 +67,24 @@ input{
     margin-bottom: 10px;
     border-radius: 5px;
     min-width: 100%;
+    background-color: brown;
+    padding: 3px 10px;
 }
 .but{
+    min-width: 130px;
     background-color: blue;
-    padding: 5px 10px;
+    padding: 10px 10px;
     border-radius: 5px;
-    width:100px;
+    text-transform: uppercase;
+
+    &:last-of-type{
+        background-color: green;
+    }
+
+    &:hover{
+        background-color:black;
+        color:whitesmoke; 
+    }
 }
 
 </style>
